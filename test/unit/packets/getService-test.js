e48af1a501588b0f6321a1ef36abe8ed2f30e3e3
Marius Rumpf <marius@mariusrumpf.de>
2015-07-03 18:40:34 +0200

    Test getService packet

---
+++ test/unit/packets/getService-test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var Packet = require('../../../').packet;
+var assert = require('chai').assert;
+
+suite('Packet getService', () => {
+  suite('create', () => {
+    test('general', () => {
+      let packet = Packet.create('getService', {}, 'ff2c4807');
+      assert.equal(packet.size, 36);
+      assert.equal(packet.type, 2);
+      assert.equal(packet.source, 'ff2c4807');
+      assert.isUndefined(packet.target);
+    });
+  });
+});
