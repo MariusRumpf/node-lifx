45c7ab8f88799486d921e6b69e618fb479bc7f0a
Marius Rumpf <marius@mariusrumpf.de>
2017-01-19 18:18:12 +0100

    Add setWaveform example

---
+++ example/setWaveform.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/**
+ * Searches for new lights, if one is found it sends a setWaveform packet
+ * which tells the light to pulse to red and back three times over a period of 800ms
+ */
+
+var LifxClient = require('../lib/lifx').Client;
+var packet = require('../lib/lifx').packet;
+var constants = require('../lib/lifx').constants;
+var client = new LifxClient();
+
+// Create our packet with for pulsing red color effect
+var packetObj = packet.create('setWaveform', {
+  isTransient: true,
+  color: {hue: 0, saturation: 65535, brightness: 65535, kelvin: 3500},
+  period: 800,
+  cycles: 3,
+  skewRatio: 0,
+  waveform: constants.LIGHT_WAVEFORMS.indexOf('SINE') // SAW, SINE, HALF_SINE, TRIANGLE, PULSE
+}, client.source);
+
+// Function running when packet was received by light
+var callback = function() {
+  console.log('Packet send\n');
+};
+
+client.on('light-new', function(light) {
+  console.log('New light found.');
+  console.log('ID: ' + light.id);
+
+  // Set the light id
+  packetObj.target = light.id; // set target id to new light
+  client.send(packetObj, callback);
+});
+
+// Give feedback when running
+client.on('listening', function() {
+  var address = client.address();
+  console.log(
+    'Started LIFX listening on ' +
+    address.address + ':' + address.port + '\n'
+  );
+});
+
+client.init();
